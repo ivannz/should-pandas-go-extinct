ea45a62fb5c1b54ca738addc906b04b32fecb672
Ivan Nazarov <ivannz@yandex.ru>
2026-09-12 09:49:56 +0000

    Add chunked pandas 1BRC profiling case

--- run_profile.sh
+++ run_profile.sh
@@ -5,3 +5,4 @@ N_WARMUP=2
 ./profile.sh ./.venv/bin/python duckdb_1brc.py $N_ITER $N_WARMUP ./data/duckdb.csv
 ./profile.sh ./.venv/bin/python polars_1brc.py $N_ITER $N_WARMUP ./data/polars.csv
 ./profile.sh ./.venv/bin/python pandas_1brc.py $N_ITER $N_WARMUP ./data/pandas.csv
+./profile.sh ./.venv/bin/python pandas_1brc_iter.py $N_ITER $N_WARMUP ./data/pandas_iter.csv
